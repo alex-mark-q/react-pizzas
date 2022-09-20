e90fe0c9acca74485d344bf664dfb5e70437e22b
alex-mark-q <61847579+alex-mark-q@users.noreply.github.com>
2022-09-20 17:34:04 +0400

    update project

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Logo.module.scss';
+
+const Logo = () => (
+  <div className={styles.Logo}>
+    Logo Component
+  </div>
+);
+
+Logo.propTypes = {};
+
+Logo.defaultProps = {};
+
+export default Logo;
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Menu.module.scss';
+
+const Menu = () => (
+  <div className={styles.Menu}>
+    Menu Component
+  </div>
+);
+
+Menu.propTypes = {};
+
+Menu.defaultProps = {};
+
+export default Menu;
